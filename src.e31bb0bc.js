93eef20b68b3f4d6222f63abe8c67b3194859244
Yegor Gangalo <gangaloyegor@gmail.com>
2020-11-24 22:15:03 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -552,7 +552,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62648" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65507" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
