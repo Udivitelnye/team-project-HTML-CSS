5b656c12d3a0de2f94c5e78baf74b9d4010b82f6
Yegor Gangalo <gangaloyegor@gmail.com>
2020-11-25 01:07:22 +0200

    made number counts in tradition section

--- src/js/btn-animation.js
+++ src/js/btn-animation.js
@@ -22,11 +22,11 @@ const countFoodRef = document.querySelector('[data-count-food]');
 const maxNumberCafe = Number(countCafeRef.textContent);
 const maxNumberFood = Number(countFoodRef.textContent);
 
-const printNumbers = (from, to, elementRef, interval) => {
+const printNumbers = (from, to, elementRef, interval, sufix='') => {
   let current = from;
 
   const inCrement = () => {
-    elementRef.textContent = current;
+    elementRef.textContent = current+sufix;
     if (current === to) {
       return;
     }
@@ -38,4 +38,32 @@ const printNumbers = (from, to, elementRef, interval) => {
 
 printNumbers(0, maxNumberCafe, countCafeRef, 150);
 printNumbers(0, maxNumberFood, countFoodRef, 100);
-/* ----------------------------------- */
+
+
+/* ----------------IntersectionObserver for Number counts------------------- */
+
+const statStatItemAray = document.querySelectorAll('[data-stat-item]');
+
+const options = {
+  // rootMargin: "-100px",
+  threshold: [0.5],
+};
+
+const ioCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const maxNumber = Number.parseInt(entry.target.textContent);
+      let sufix;
+      if (entry.target.textContent.includes('kg')) {
+         sufix = 'kg';
+        }
+      let from = maxNumber - 50;
+      if (from < 0) { from = 0 };
+      printNumbers(from, maxNumber, entry.target, 30, sufix);
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const intersecOb = new IntersectionObserver(ioCallback, options);
+statStatItemAray.forEach(item => intersecOb.observe(item));
