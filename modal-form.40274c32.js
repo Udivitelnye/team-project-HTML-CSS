91536a09b934a94e83412c0f5089923d372f7997
Yegor Gangalo <gangaloyegor@gmail.com>
2020-11-27 10:18:23 +0200

    Updates

--- modal-form.40274c32.js
+++ modal-form.40274c32.js
@@ -174,8 +174,11 @@ function modalFocus() {
 var checkboxIconRef = document.querySelector('[data-checkbox-icon] svg');
 var checkboxInputRef = document.querySelector('[data-checkbox-input]');
 var checkboxLabelRef = document.querySelector('[data-checkbox-label]');
-checkboxLabelRef.addEventListener('click', byClickChecked);
 checkboxIconRef.addEventListener('focus', addEventListenerAddChecked());
+checkboxLabelRef.addEventListener('click', function (event) {
+  return event.preventDefault();
+});
+checkboxLabelRef.addEventListener('click', byClickChecked);
 
 function addEventListenerAddChecked() {
   window.addEventListener('keydown', onPressEnterAdd);
@@ -328,6 +331,12 @@ var checkedInput = function checkedInput(event) {
 modalInputArray.forEach(function (input) {
   return input.addEventListener('input', _.debounce(checkedInput, 500));
 });
+/* ----------------------------- */
+// const labelRef = document.querySelector('label');
+// labelRef.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   console.log(e.preventDefault());
+// });
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -356,7 +365,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53157" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52314" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
